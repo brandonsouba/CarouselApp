a702fce1d5ed1c7ffdb73b3a3c060df1fdb4de39
brandonsouba <brandonsouba@brandon.local>
2015-05-13 16:31:30 -0700

    Initial Commit

---
+++ CarouselApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarouselApp
+//
+//  Created by Brandon Souba on 5/13/15.
+//  Copyright (c) 2015 BrandonSOuba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
